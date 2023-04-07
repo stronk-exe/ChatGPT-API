18675eaa238e6d92340c28ea3a982eb866c97a84
Abderrahman El asri <ael-asri@e2r3p9.1337.ma>
2023-04-07 15:26:08 +0000

    react chat engine setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Chat from "@/components/chat"
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Rout path='/chat' element={<CharacterData />} />
+          <Route path='/chat' element={<Chat />} />
         </Routes>
       </BrowserRouter>
     </div>
